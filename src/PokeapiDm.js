515fbec032c546b309f58bd6715e061f9cf61137
Diego Andres Mateus Triana <diego.mateus@novatec.com.co>
2024-07-12 17:02:33 -0500

    Update test

--- src/PokeapiDm.js
+++ src/PokeapiDm.js
@@ -15,87 +15,74 @@ import '@bbva-global-apis-dm/bbva-global-generic-dm';
 export class PokeapiDm extends LitElement {
   static get properties() {
     return {
-      params: { type: Object },
+      host: { type: String },
+      path: { type: String },
     };
   }
 
   constructor() {
     super();
-    this.params = {
-      path: ''
-    };
+    this.path = '';
   }
 
   render() {
     return html`
-      <p>Welcome to</p>
       <bbva-global-generic-dm
         id="el"
-        method="GET"
         @request-success="${this._getPokemonSuccess}"
         @request-error="${this._getPokemonError}"
-        ></bbva-global-generic-dm>
-        <bbva-global-generic-dm
+      ></bbva-global-generic-dm>
+      <bbva-global-generic-dm
         id="el2"
-        method="GET"
         @request-success="${this._getPokemonInfoSuccess}"
         @request-error="${this._getPokemonInfoError}"
-        ></bbva-global-generic-dm>
-      <button
-        class="btn btn-primary"
-        type="button"
-        @click="${this.getPokemonData}"
-      >
-        Text
-      </button>
+      ></bbva-global-generic-dm>
     `;
-  }
-
-  _getPokemonSuccess(success) {
-    console.log(success.detail);
-    this.dispatchEvent(new CustomEvent('get-pokemon-success', {
-      detail: success.detail
-    }));
-  }
-
-  _getPokemonError(error) {
-    console.log(error.detail);
-    this.dispatchEvent(new CustomEvent('get-pokemon-error', {
-      detail: error.detail
-    }));
-  }
-
-  _getPokemonInfoSuccess(success) {
-    console.log(success.detail);
-    this.dispatchEvent(new CustomEvent('get-pokemon-info-success', {
-      detail: success.detail
-    }));
-  }
-
-  _getPokemonInfoError(error) {
-    console.log(error.detail);
-    this.dispatchEvent(new CustomEvent('get-pokemon-info-error', {
-      detail: error.detail
-    }));
   }
 
   async getPokemonData() {
     await this.requestUpdate();
-    const genericDM = this .shadowRoot.querySelector('#el');
-    genericDM.path = this.params.path;
-    genericDM.host = 'https://pokeapi.co/api/v2/pokemon/';
+    const genericDM = this.shadowRoot.querySelector("#el");
+    genericDM.method = "GET";
+    genericDM.host = "https://pokeapi.co/api/v2/pokemon/";
     genericDM.crossDomain = false;
     genericDM.getData();
   }
 
   async getPokemonInfo() {
     await this.requestUpdate();
-    const genericDM = this .shadowRoot.querySelector('#el2');
-    genericDM.path = this.params.path;
-    genericDM.host = 'https://pokeapi.co/api/v2/pokemon/';
+    const genericDM = this.shadowRoot.querySelector("#el2");
+    genericDM.path = this.path;
+    genericDM.method = "GET";
+    genericDM.host = "https://pokeapi.co/api/v2/pokemon/";
     genericDM.crossDomain = false;
     genericDM.getData();
   }
 
+  _getPokemonSuccess(success) {
+    console.log(success.detail);
+    this._fireEvent("get-pokemon-success", success.detail);
+  }
 
+  _getPokemonError(error) {
+    this._fireEvent("get-pokemon-error", error.detail);
+  }
+
+  _getPokemonInfoSuccess(success) {
+    this._fireEvent("get-pokemon-info-success", success.detail);
+  }
+
+  _getPokemonInfoError(error) {
+    this._fireEvent("get-pokemon-info-error", error.detail);
+  }
+
+  _fireEvent(event, detail) {
+    this.dispatchEvent(
+      new CustomEvent(event, {
+        bubbles: true,
+        composed: true,
+        detail: detail,
+      })
+    );
+  }
 }
